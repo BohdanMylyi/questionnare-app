3616e275dd3c1a2bda64bd284e4be89e57dd06d2
Bohdan Mylyi <mylyibohdan18@gmail.com>
2025-03-18 12:43:34 +0200

    add: drag and drop

--- app-frontend/src/pages/CreateQuestionnare.jsx
+++ app-frontend/src/pages/CreateQuestionnare.jsx
@@ -92,19 +92,23 @@ export default function CreateQuestionnaire() {
             {questions.map((q, qIndex) => (
               <div key={qIndex} className="flex flex-col items-center gap-2">
                 <div className="flex flex-col gap-1">
-                  <h3 className="font-bold">Question {qIndex + 1}:</h3>
+                  <h3 className="font-bold my-1">Question {qIndex + 1}:</h3>
                   <h3 className="font-bold">Questions:</h3>
                   <input
                     type="text"
                     placeholder="Question"
                     className="font-bold bg-[#fff] border-2 p-1 rounded-sm"
-                    {...register("question", {
+                    {...register(`questions.${qIndex}.text`, {
                       required: "Question is required!",
                     })}
+                    value={q.text}
+                    onChange={(e) =>
+                      updateQuestion(qIndex, "text", e.target.value)
+                    }
                   />
-                  {errors.question === "required" && (
+                  {errors.questions?.[qIndex]?.text && (
                     <p role="error" className="text-red-600">
-                      {errors.question.message}
+                      {errors.questions[qIndex].text.message}
                     </p>
                   )}
                 </div>
@@ -125,9 +129,8 @@ export default function CreateQuestionnaire() {
                 {q.type !== "text" && (
                   <div>
                     {q.choices.map((c, cIndex) => (
-                      <div className="flex flex-col my-2">
+                      <div className="flex flex-col my-2" key={cIndex}>
                         <input
-                          key={cIndex}
                           type="text"
                           placeholder="Choice"
                           value={c}
@@ -141,6 +144,7 @@ export default function CreateQuestionnaire() {
                     <button
                       className="border-2 px-5 rounded-xl bg-[#fff] mx-auto"
                       onClick={() => addChoice(qIndex)}
+                      type="button"
                     >
                       Add choice
                     </button>
@@ -152,12 +156,13 @@ export default function CreateQuestionnaire() {
               <button
                 className="border-2 px-5 rounded-xl bg-[#fff]"
                 onClick={addQuestion}
+                type="button"
               >
                 Add question
               </button>
               <button
+                type="submit"
                 className="border-2 px-5 rounded-xl bg-[#fff]"
-                onClick={handleSubmit}
               >
                 Submit
               </button>
--- app-frontend/src/pages/QuestionnaireCatalog.jsx
+++ app-frontend/src/pages/QuestionnaireCatalog.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 export default function QuestionnaireCatalog() {
   const [questionnaires, setQuestionnaires] = useState([]);
@@ -89,8 +90,33 @@ export default function QuestionnaireCatalog() {
     }
   };
 
+  const onDragEnd = async (result) => {
+    const { source, destination } = result;
+    if (!destination) return;
+
+    const reorderedQuestionnaires = Array.from(questionnaires);
+    const [removed] = reorderedQuestionnaires.splice(source.index, 1);
+    reorderedQuestionnaires.splice(destination.index, 0, removed);
+
+    try {
+      const res = await axios.put(
+        "http://localhost:5000/api/questionnaires",
+        {
+          order: reorderedQuestionnaires.map((q) => q._id),
+        }
+      );
+
+      if (res.status === 200) {
+        setQuestionnaires(reorderedQuestionnaires);
+      }
+    } catch (error) {
+      setError("Error updating order");
+      console.error("Error updating order:", error);
+    }
+  };
+
   return (
-    <div className="h-full ">
+    <div className="h-full">
       <div className="flex flex-row justify-between m-4">
         <h1 className="text-xl font-bold">Quiz Catalog</h1>
         <Link to={"/create"}>
@@ -108,93 +134,117 @@ export default function QuestionnaireCatalog() {
         </select>
       </div>
       {loading && <p>Loading...</p>}
-      <div className="w-[80%] h-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-x-hidden">
-        {sortedQuestionnaires.map((q) => (
-          <div
-            key={q._id}
-            className="card bg-[#FDFAF6] card-xs shadow-md rounded-xl p-5 border-2 "
-          >
-            <form onSubmit={handleSubmit(handleSave)}>
-              <div className="flex flex-col gap-2">
-                {editingQuestionnaire === q._id ? (
-                  <>
-                    <div className="flex flex-col gap-1">
-                      <h3 className="font-bold">Name:</h3>
-                      <input
-                        name="name"
-                        placeholder="Quiz Name"
-                        className="font-normal bg-[#fff] border-2 p-2 rounded-xl"
-                        {...register("name", {
-                          required: "Name is required!",
-                          maxLength: { value: 10, message: "Max length is 10" },
-                        })}
-                      />
-                      {errors.name && (
-                        <p role="alert" className="text-red-600">
-                          {errors.name.message}
-                        </p>
-                      )}
+
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="questionnaires">
+          {(provided) => (
+            <div
+              className="w-[80%] h-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-x-hidden"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {sortedQuestionnaires.map((q, index) => (
+                <Draggable key={q._id} draggableId={q._id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="card bg-[#FDFAF6] card-xs shadow-md rounded-xl p-5 border-2"
+                    >
+                      <form onSubmit={handleSubmit(handleSave)}>
+                        <div className="flex flex-col gap-2">
+                          {editingQuestionnaire === q._id ? (
+                            <>
+                              <div className="flex flex-col gap-1">
+                                <h3 className="font-bold">Name:</h3>
+                                <input
+                                  name="name"
+                                  placeholder="Quiz Name"
+                                  className="font-normal bg-[#fff] border-2 p-2 rounded-xl"
+                                  {...register("name", {
+                                    required: "Name is required!",
+                                    maxLength: {
+                                      value: 10,
+                                      message: "Max length is 10",
+                                    },
+                                  })}
+                                />
+                                {errors.name && (
+                                  <p role="alert" className="text-red-600">
+                                    {errors.name.message}
+                                  </p>
+                                )}
+                              </div>
+                              <div className="flex flex-col gap-1">
+                                <h3 className="font-bold">Description:</h3>
+                                <textarea
+                                  name="description"
+                                  placeholder="Quiz Description"
+                                  className="font-normal bg-[#fff] border-2 p-2 rounded-xl"
+                                  {...register("description", {
+                                    required: "Description is required!",
+                                    maxLength: {
+                                      value: 50,
+                                      message: "Max length is 50",
+                                    },
+                                  })}
+                                />
+                                {errors.description && (
+                                  <p role="alert" className="text-red-600">
+                                    {errors.description.message}
+                                  </p>
+                                )}
+                              </div>
+                              <div className="flex flex-row justify-between mt-4">
+                                <button
+                                  className="border-2 px-5 rounded-xl bg-[#fff]"
+                                  type="submit"
+                                >
+                                  Save
+                                </button>
+                                <button
+                                  className="border-2 px-5 rounded-xl bg-[#fff]"
+                                  onClick={() => setEditingQuestionnaire(null)}
+                                >
+                                  Cancel
+                                </button>
+                              </div>
+                            </>
+                          ) : (
+                            <div className="flex flex-col gap-2">
+                              <h3 className="text-xl font-bold">{q.name}</h3>
+                              <p>{q.description}</p>
+                              <p className="text-blue-500">
+                                Questions: {q.questions.length}
+                              </p>
+                              <div className="flex flex-row justify-between mt-4">
+                                <button
+                                  className="border-2 px-5 rounded-xl bg-[#fff]"
+                                  onClick={() => handleEdit(q)}
+                                >
+                                  Edit
+                                </button>
+                                <button
+                                  className="border-2 px-5 rounded-xl bg-[#fff]"
+                                  onClick={() => handleDelete(q._id)}
+                                >
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                      </form>
                     </div>
-                    <div className="flex flex-col gap-1">
-                      <h3 className="font-bold">Description:</h3>
-                      <textarea
-                        name="description"
-                        placeholder="Quiz Description"
-                        className="font-normal bg-[#fff] border-2 p-2 rounded-xl"
-                        {...register("description", {
-                          required: "Description is required!",
-                          maxLength: { value: 50, message: "Max length is 50" },
-                        })}
-                      />
-                      {errors.description && (
-                        <p role="alert" className="text-red-600">
-                          {errors.description.message}
-                        </p>
-                      )}
-                    </div>
-                    <div className="flex flex-row justify-between mt-4">
-                      <button
-                        className="border-2 px-5 rounded-xl bg-[#fff]"
-                        type="submit"
-                      >
-                        Save
-                      </button>
-                      <button
-                        className="border-2 px-5 rounded-xl bg-[#fff]"
-                        onClick={() => setEditingQuestionnaire(null)}
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  </>
-                ) : (
-                  <div className="flex flex-col gap-2">
-                    <h3 className="text-xl font-bold">{q.name}</h3>
-                    <p>{q.description}</p>
-                    <p className="text-blue-500">
-                      Questions: {q.questions.length}
-                    </p>
-                    <div className="flex flex-row justify-between mt-4">
-                      <button
-                        className="border-2 px-5 rounded-xl bg-[#fff]"
-                        onClick={() => handleEdit(q)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        className="border-2 px-5 rounded-xl bg-[#fff]"
-                        onClick={() => handleDelete(q._id)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </form>
-          </div>
-        ))}
-      </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
